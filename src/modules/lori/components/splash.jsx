2d0fb1ee698a3c23aacadaa8febc9f567c3edf91
Daniel Barde <mrbarde2k@gmail.com>
2017-08-20 18:13:58 +0100

    remove particlejs

--- src/modules/lori/components/splash.jsx
+++ src/modules/lori/components/splash.jsx
@@ -44,15 +44,6 @@ class Home extends Component{
 					author: 'Plato'
 				}
 			],
-            particleStyles: {
-				position: 'absolute',
-				left: 0,
-				top: 0,
-				width: '100%',
-				height: '100%',
-				zIndex: 0,
-				opacity: 0.4
-			},
 			quote: null
 		};
 	}
@@ -69,12 +60,11 @@ class Home extends Component{
 	}
 
 	render(){
-		var {quote, particleStyles} = this.state;
+		var {quote} = this.state;
 		// return view
 		return(
 			<Page title="Welcome to Lori Framework">
 				<div styleName='spanned'>
-					<Particles style={particleStyles} className="foo" params={require('../files/particlesjs-config.json')}/>
 					<div styleName='column'>
 
 					</div>
